cf9beb982d9a5c0ecb49679810e3677c7114dac3
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-16 16:28:23 +0600

    solved problem contains duplicate

---
+++ src/main/java/kz/baltabayev/leetcode/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+package kz.baltabayev.leetcode;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
