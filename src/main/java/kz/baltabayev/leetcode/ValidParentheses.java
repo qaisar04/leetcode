ac4fd12edddb894b5ef1dca2f390c73d6952c1c5
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-17 22:29:20 +0600

    ValidParentheses Stack

---
+++ src/main/java/kz/baltabayev/leetcode/ValidParentheses.java
@@ -0,0 +1,28 @@
+package kz.baltabayev.leetcode;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if (s == null || s.isEmpty()) {
+            return true;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
