a718743c64db97ac690cd3627ed423314f2a2202
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-10-03 07:20:34 +0300

    BinarySearch

--- src/main/java/kz/baltabayev/leetcode/BinarySearch.java
+++ src/main/java/kz/baltabayev/leetcode/BinarySearch.java
@@ -2,12 +2,24 @@ package kz.baltabayev.leetcode;
 
 public class BinarySearch {
     public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == target) {
-                return i;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int middleValue = nums[middle];
+
+            if (middleValue == target) {
+                return middle; // target found
+            }
+
+            if (middleValue < target) {
+                left = middle + 1; // search right half
+            } else {
+                right = middle - 1; // search left half
             }
         }
 
-        return -1;
+        return -1; // target not found
     }
 }
