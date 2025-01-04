3cbdd84459e3c69a3315b93640f111b5d2ba1571
Qaisar Baltabayev <q.baltabayev@icloud.com>
2025-01-04 18:10:53 +0300

    167. Two Sum II - Input Array Is Sorted

---
+++ src/main/java/kz/baltabayev/leetcode/TwoSumII.java
@@ -0,0 +1,32 @@
+package kz.baltabayev.leetcode;
+
+import java.util.Arrays;
+
+public class TwoSumII {
+
+    public static void main(String[] args) {
+        TwoSumII twoSum = new TwoSumII();
+        int[] nums = {2,7,11,15};
+        int[] result = twoSum.twoSum(nums, 9);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[] {0, 0};
+    }
+}
