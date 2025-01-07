4779ea57f14344729a417d260413f4cbe4d3e28c
Qaisar Baltabayev <q.baltabayev@icloud.com>
2025-01-07 14:02:32 +0300

    1. Two Sum

--- src/main/java/kz/baltabayev/leetcode/TwoSum.java
+++ src/main/java/kz/baltabayev/leetcode/TwoSum.java
@@ -1,18 +1,20 @@
 package kz.baltabayev.leetcode;
 
-public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+import java.util.HashMap;
 
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                }
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
-        //todo: increase productivity
-        return result;
+
+        return new int[] {};
     }
 }
