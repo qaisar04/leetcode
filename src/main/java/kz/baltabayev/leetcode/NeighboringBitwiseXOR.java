a7aa254fac5fd96f89f4810b24c786de818a3f04
Qaisar Baltabayev <q.baltabayev@icloud.com>
2025-01-17 08:49:09 +0300

    2683. Neighboring Bitwise XOR

--- src/main/java/kz/baltabayev/leetcode/NeighboringBitwiseXOR.java
+++ src/main/java/kz/baltabayev/leetcode/NeighboringBitwiseXOR.java
@@ -3,16 +3,17 @@ package kz.baltabayev.leetcode;
 public class NeighboringBitwiseXOR {
 
     public boolean doesValidArrayExist(int[] derived) {
-        int[] original = new int[derived.length];
+        int n = derived.length;
+        int[] original = new int[n];
 
-        for (int i = 1; i < derived.length; i++) {
+        for (int i = 1; i < n; i++) {
             original[i] = derived[i - 1] ^ original[i - 1];
         }
 
-        for (int i = 0; i < derived.length; i++) {
+        for (int i = 0; i < n; i++) {
             int xor;
 
-            if (i == derived.length - 1) {
+            if (i == n - 1) {
                 xor = original[i] ^ original[0];
             } else {
                 xor = original[i] ^ original[i + 1];
