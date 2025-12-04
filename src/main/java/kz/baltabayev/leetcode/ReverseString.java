100b0a4aab0f6c9acb239b5a646d9b9241206483
Kaisar Baltabaev <k.baltabaev@kpecom.kz>
2025-12-04 15:59:59 +0500

    add: implement solution for reversing a string in place

---
+++ src/main/java/kz/baltabayev/leetcode/ReverseString.java
@@ -0,0 +1,18 @@
+package kz.baltabayev.leetcode;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left += 1;
+            right -= 1;
+        }
+    }
+}
