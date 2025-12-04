39a3025bd26f4c9ccb19315161454a0ff35f2dbe
Kaisar Baltabaev <k.baltabaev@kpecom.kz>
2025-12-04 15:59:34 +0500

    refactor: optimize palindrome checking by using two-pointer technique

--- src/main/java/kz/baltabayev/leetcode/ValidPalindrom.java
+++ src/main/java/kz/baltabayev/leetcode/ValidPalindrom.java
@@ -9,10 +9,25 @@ public class ValidPalindrom {
     }
 
     public boolean isPalindrome(String s) {
-        String originalString = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-        StringBuilder stringBuffer = new StringBuilder(originalString);
-        StringBuilder reversedString = stringBuffer.reverse();
+        int left = 0;
+        int right = s.length() - 1;
 
-        return originalString.equals(new String(reversedString));
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
