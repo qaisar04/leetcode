3970095cee981bdc0eef47753ad078d69aa5b53e
Балтабаев Кайсар Бегисович <kaisar.baltabayev@kaspi.kz>
2026-01-14 17:39:26 +0500

    add: implement solution for removing all adjacent duplicates in a string

---
+++ src/main/java/kz/baltabayev/leetcode/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,22 @@
+package kz.baltabayev.leetcode;
+
+import java.util.Stack;
+
+public class RemoveAllAdjacentDuplicatesInString {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        char[] res = new char[stack.size()];
+        for (int i = 0; i < stack.size(); i++) {
+            res[i] = stack.get(i);
+        }
+        return new String(res);
+    }
+}
