3e0e1c736092d18514a6a262c5d5ad08eda321fc
Балтабаев Кайсар Бегисович <qaisar12300@gmail.com>
2026-02-20 14:39:50 +0500

    add: implement solution for binary tree level order traversal (BFS)

---
+++ src/main/java/kz/baltabayev/leetcode/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,35 @@
+package kz.baltabayev.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import kz.baltabayev.leetcode.utils.TreeNode;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> level = new ArrayList<>(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+}
